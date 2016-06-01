1b5360a45c77378829a7c206f71446b89c8d1d58
Laurie Cuffney <Laurie Cuffney>
2016-05-31 23:28:28 -0400

    simplified functions and added create_art function

--- draw_shapes.py
+++ draw_shapes.py
@@ -5,42 +5,51 @@ import turtle
 window = turtle.Screen()
 window.bgcolor("black")
 
-def draw_square(side_length):
-    jeff = turtle.Turtle() # get a turtle
-    jeff.shape("turtle") # make jeff a real turtle
-    jeff.color("green") # make jeff a yellow turtle
-    jeff.speed(20)
+def draw_square(artist_turtle, side_length):
+    artist_turtle.speed(20)
     # tell jeff where to to
     deg0 = 0
     while deg0 <360:
         deg1 = 0
         while deg1 <360:
-            jeff.forward(side_length)
-            jeff.right(90)
+            artist_turtle.forward(side_length)
+            artist_turtle.right(90)
             deg1 += 90
         deg0 += 10
-        jeff.right(10)
+        artist_turtle.right(10)
 
 
-def draw_circle(radius):
-    jody = turtle.Turtle() # get another turtle
-    jody.shape('turtle')
-    jody.color('blue')
-    jody.setpos(0,-100)
-    jody.circle(radius)
+def draw_circle(artist_turtle, radius):
+    artist_turtle.setpos(0,-100)
+    artist_turtle.circle(radius)
 
-def draw_equilateral_triangle(side_length):
+def draw_equilateral_triangle(artist_turtle, side_length):
+    deg = 0
+    while deg<360:
+        artist_turtle.forward(side_length)
+        artist_turtle.right(120)
+        deg += 120
+
+def create_art():
+    # Create jeff the turtle
+    jeff = turtle.Turtle() # get a turtle
+    jeff.shape("turtle") # make jeff a real turtle
+    jeff.color("green") # make jeff a yellow turtle
+
+    # Create betty the turtle
     betty = turtle.Turtle()
     betty.shape('turtle')
     betty.color('white')
-    deg = 0
-    while deg<360:
-        betty.forward(side_length)
-        betty.right(120)
-        deg += 120
 
-draw_square(100)
-draw_circle(100)
-draw_equilateral_triangle(100)
+    # Create jody the turtle
+    jody = turtle.Turtle() # get another turtle
+    jody.shape('turtle')
+    jody.color('blue')
+
+    draw_square(jeff, 100)
+    draw_circle(jody, 100)
+    draw_equilateral_triangle(betty, 100)
+
+create_art()
 
 window.exitonclick()
